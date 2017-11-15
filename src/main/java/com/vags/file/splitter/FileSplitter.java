397641add8cc0433b2110d7fde58cc2e1bf835b1
vageesh <vageeshs7@gmail.com>
2017-11-15 23:57:05 +0530

    Command line and UI support.

--- src/main/java/com/vags/file/splitter/FileSplitter.java
+++ src/main/java/com/vags/file/splitter/FileSplitter.java
@@ -1,5 +1,6 @@
 package com.vags.file.splitter;
 
+import javax.swing.*;
 import javax.swing.plaf.synth.SynthEditorPaneUI;
 import java.io.*;
 
@@ -61,26 +62,58 @@ public class FileSplitter {
     public static void main(String[] args)
     {
         FileSplitter fs = new FileSplitter();
+        if(args.length > 0 && "-n".equals(args[0])) {
+            fs.handleCommandLineRequest(args[1], args[2]);
+        }else {
+            fs.handleGUIRequest();
+        }
+    }
+
+    private void handleCommandLineRequest(String filename, String size) {
         long bytesPerSplit = 0;
 
         try {
-            if(args[1].endsWith("KB") || args[1].endsWith("kb"))
+            if(size.endsWith("KB") || size.endsWith("kb"))
             {
-                String lengthStr = args[1].substring(0, args[1].length()-2);
+                String lengthStr = size.substring(0, size.length()-2);
                 bytesPerSplit = Long.parseLong(lengthStr.trim());
                 bytesPerSplit = bytesPerSplit * 1024; //kilo bytes
-            } else if(args[1].endsWith("MB") || args[1].endsWith("mb"))
+            } else if(size.endsWith("MB") || size.endsWith("mb"))
             {
-                String lengthStr = args[1].substring(0, args[1].length()-2);
+                String lengthStr = size.substring(0, size.length()-2);
                 bytesPerSplit = Long.parseLong(lengthStr.trim());
                 bytesPerSplit = bytesPerSplit * 1024 * 1024; //kilo bytes
             }else{
-                bytesPerSplit = Long.parseLong(args[1].trim());
+                bytesPerSplit = Long.parseLong(size.trim());
             }
 
-            fs.splitTextFile(args[0], bytesPerSplit);
+            splitTextFile(filename, bytesPerSplit);
         } catch (NumberFormatException e) {
             e.printStackTrace();
         }
+    }
+
+    private void handleGUIRequest()
+    {
+        System.out.println("Starting File Splitter UI");
+        JFrame frame = new JFrame("VS File Splitter");
+
+        JTextField filenameField = new JTextField();
+        filenameField.setBounds(20, 20, 150, 20);
+
+        JTextField sizeField = new JTextField();
+        sizeField.setBounds(20, 50, 150, 20);
+
+        JButton splitButton = new JButton(" Split ");
+        splitButton.setBounds(20, 80, 150, 20);
+
+        frame.add(filenameField);
+        frame.add(sizeField);
+        frame.add(splitButton);
+
+        frame.setSize(300, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setVisible(true);
     }
 }
